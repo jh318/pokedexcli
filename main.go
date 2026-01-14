68a9fc5bafe2406096c7a594946a0158d5d6249b
jh318 <jhawley318@gmail.com>
2026-01-14 10:56:23 -0800

    Added REPL loop to main

--- main.go
+++ main.go
@@ -2,9 +2,21 @@ package main
 
 import "fmt"
 import "strings"
+import "bufio"
+import "os"
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		inputFormatted := cleanInput(input)
+		if len(inputFormatted) == 0 {
+			continue
+		}
+		fmt.Printf("Your command was: %v\n", inputFormatted[0])
+	}
 }
 
 func cleanInput(text string) []string {
