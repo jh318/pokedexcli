71439b4d52c77468225e6686407c7b2662d207d0
jh318 <jhawley318@gmail.com>
2026-01-15 09:58:55 -0800

    added help text for map and mapb

--- command_help.go
+++ command_help.go
@@ -8,5 +8,7 @@ func commandHelp(cfg *config) error {
 	fmt.Println()
 	fmt.Println("help: Displays a help message")
 	fmt.Println("exit: Exit the Pokedex")
+	fmt.Println("map: Displays the next set of locations")
+	fmt.Println("mapb: Displays the previous set of locations")
 	return nil
 }
