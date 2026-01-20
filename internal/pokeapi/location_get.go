26158fa11f716aa8cd272481056e1788ae1e4756
jh318 <jhawley318@gmail.com>
2026-01-20 15:28:45 -0800

    Completed adding explore command.

--- internal/pokeapi/location_get.go
+++ internal/pokeapi/location_get.go
@@ -1,11 +1,36 @@
 package pokeapi
 
+import (
+    "encoding/json"
+    "io"
+)
+
 func (c *Client) GetLocation(locationName string) (Location, error) {
-	if data, ok : c.cache.Get(locationName); ok {
+	url := baseURL + "/location-area/" + locationName
+	if data, ok := c.cache.Get(url); ok {
 		var location Location
-		if err := json.Unmarshal(data, &locations); err != nil {
+		if err := json.Unmarshal(data, &location); err != nil {
 			return Location{}, err
 		}
-		return locations, nil
+		return location, nil
 	}
+
+	res, err := c.httpClient.Get(url)
+	if err != nil {
+		return Location{}, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return Location{}, err
+	}
+
+	var location Location
+	if err := json.Unmarshal(body, &location); err != nil {
+		return Location{}, err
+	}
+
+	c.cache.Add(url, body)
+	return location, nil
 }
